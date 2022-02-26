ae07a54330870f2daa0e31621f949b7d8c71357f
Thunraz <thunraz@thunraz.com>
2022-02-26 15:25:20 +0100

    Fix cout spam for missing animation

--- impl/gamelib/character.cpp
+++ impl/gamelib/character.cpp
@@ -135,14 +135,17 @@ void PlayerCharacter::createAnimation()
             { 51, 52, 53, 54, 55 }, 0.1f, getGame()->gfx().textureManager());
     }
 
+    m_animation->play("idle");
+    m_animation->setPosition(jt::Vector2f { 5 * 24, 7 * 24 });
+
     m_attackUnderlay = std::make_shared<jt::Animation>();
     m_attackUnderlay->setLooping(false);
+    m_attackUnderlay->add("assets/attack_underlay.png", "initial", jt::Vector2u { 32u, 32u }, { 0 },
+        0.1f, getGame()->gfx().textureManager());
     m_attackUnderlay->add("assets/attack_underlay.png", "attack", jt::Vector2u { 32u, 32u },
         jt::MathHelper::numbersBetween(0u, 9u), frameTimeAttack * 2.0f,
         getGame()->gfx().textureManager());
-
-    m_animation->play("idle");
-    m_animation->setPosition(jt::Vector2f { 5 * 24, 7 * 24 });
+    m_attackUnderlay->play("initial");
 }
 
 void PlayerCharacter::doUpdate(float const elapsed)
