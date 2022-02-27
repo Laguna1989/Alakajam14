8a0ce0278a588ea0ebe725367ecf8f6a8ecf27fd
Thunraz <thunraz@thunraz.com>
2022-02-27 18:42:43 +0100

    Play sound on key pickup

--- impl/gamelib/key.cpp
+++ impl/gamelib/key.cpp
@@ -4,17 +4,22 @@
 #include "math_helper.hpp"
 #include "stairs.hpp"
 #include "state_game.hpp"
+
 Key::Key(jt::Vector2f const& pos, StateGame& state)
     : m_pos(pos)
     , m_state(state)
 {
-    std::cout << "key: " << m_pos.x << ", " << m_pos.y << std::endl;
 }
+
 void Key::doCreate()
 {
     m_sprite = std::make_shared<jt::Sprite>("assets/key.png", getGame()->gfx().textureManager());
     m_sprite->setPosition(m_pos);
+
+    m_soundPickup = std::make_shared<jt::Sound>("assets/sound/pickup_key.ogg");
+    m_soundDoorUnlock = std::make_shared<jt::Sound>("assets/sound/door.ogg");
 }
+
 void Key::doUpdate(const float elapsed)
 {
     m_sprite->update(elapsed);
@@ -26,10 +31,15 @@ void Key::doUpdate(const float elapsed)
             m_sprite->getLocalBounds().width / 2.0f }
         - player->getPosition() + GP::PlayerSize() / 2.0f);
 
-    if (d < 20.0f) {
+    if (d < 20.0f && m_locked) {
         door->unlock();
-        // TODO: Play sound
+        m_locked = false;
+
+        m_soundPickup->play();
+        m_soundDoorUnlock->play();
+
         m_sprite->setColor(jt::colors::Transparent);
     }
 }
+
 void Key::doDraw() const { m_sprite->draw(getGame()->gfx().target()); }
--- impl/gamelib/key.hpp
+++ impl/gamelib/key.hpp
@@ -1,8 +1,10 @@
 #ifndef ALAKAJAM14_KEY_HPP
 #define ALAKAJAM14_KEY_HPP
 
+#include "audio/sound.hpp"
 #include "game_object.hpp"
 #include "sprite.hpp"
+
 class StateGame;
 class Key : public jt::GameObject {
 
@@ -10,11 +12,17 @@ public:
     Key(jt::Vector2f const& pos, StateGame&);
 
 private:
-    bool m_locked = false;
+    bool m_locked { true };
+
     void doCreate() override;
     void doUpdate(float const /*elapsed*/) override;
     void doDraw() const override;
+
     std::shared_ptr<jt::Sprite> m_sprite;
+
+    std::shared_ptr<jt::Sound> m_soundPickup;
+    std::shared_ptr<jt::Sound> m_soundDoorUnlock;
+
     jt::Vector2f m_pos;
     StateGame& m_state;
 };
