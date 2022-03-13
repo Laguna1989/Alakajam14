69bcd286680fe2a93cfed455a26647c4ce3a55b2
Simon Weis <laguna_1989@gmx.net>
2022-03-13 13:52:03 +0100

    implement update functionality for eventqueue

--- impl/jamtemplate/common/audio/audio_event_queue.cpp
+++ impl/jamtemplate/common/audio/audio_event_queue.cpp
@@ -1,1 +1,29 @@
 #include "audio_event_queue.hpp"
+#include "sound.hpp"
+void AudioEventQueue::update(float elapsed)
+{
+    checkForNewEvents();
+    updateSounds(elapsed);
+}
+
+void AudioEventQueue::updateSounds(float elapsed)
+{
+    for (auto& kvp : m_sounds) {
+        kvp.second->update(elapsed);
+    }
+}
+
+void AudioEventQueue::checkForNewEvents()
+{
+    while (!m_buffer.empty()) {
+        auto event = m_buffer.back();
+        m_buffer.pop_back();
+
+        auto const fileName = event.AudioFileName;
+        if (m_sounds.count(fileName) == 0) {
+            auto snd = std::make_shared<jt::Sound>(fileName);
+            m_sounds[fileName] = snd;
+        }
+        m_sounds[fileName]->play();
+    }
+}
--- impl/jamtemplate/common/audio/audio_event_queue.hpp
+++ impl/jamtemplate/common/audio/audio_event_queue.hpp
@@ -2,13 +2,22 @@
 #define ALAKAJAM14_AUDIO_EVENT_QUEUE_HPP
 
 #include "audio_play_event.hpp"
+#include "sound_interface.hpp"
+#include <map>
+#include <memory>
+#include <string>
 #include <vector>
 
 class AudioEventQueue {
 public:
+    void update(float elapsed);
 
 private:
     std::vector<AudioPlayEvent> m_buffer;
+
+    std::map<std::string, std::shared_ptr<jt::SoundInterface>> m_sounds;
+    void checkForNewEvents();
+    void updateSounds(float elapsed);
 };
 
 #endif // ALAKAJAM14_AUDIO_EVENT_QUEUE_HPP
